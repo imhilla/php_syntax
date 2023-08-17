7ece37e1181c6f6fea87260382ba9fe407446dd0
imhilla <hillaryodhiambo282@gmail.com>
2023-08-17 20:16:43 +0300

    string references

---
+++ 06_strings.php
@@ -0,0 +1,11 @@
+<?php
+// string length
+echo strlen("Hello world!");
+// word count
+echo str_word_count("Hello world");
+// string reverse
+echo strrev("Hello world");
+// search for position
+echo strpos("Hello world", "world");
+// string replace
+echo str_replace("world", "Dolly", "Hello world");
