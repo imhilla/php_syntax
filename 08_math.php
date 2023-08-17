177ff14bdc40a4e4e2d03537033d7a62e6b2675a
imhilla <hillaryodhiambo282@gmail.com>
2023-08-17 20:48:32 +0300

    numbers

---
+++ 08_math.php
@@ -0,0 +1,23 @@
+<?php
+echo pi();
+echo "<br>";
+
+// min
+echo min(0, 150, 30, 20, -8, -200);
+echo "<br>";
+echo max(0, 150, 30, 20, -8, -200);
+
+// abs
+echo abs(-6.7);
+echo "<br>";
+
+//sqrt
+echo sqrt(64);
+echo "<br>";
+
+// round
+echo round(0.49);
+echo "<br>";
+
+// rand
+echo rand(10, 100);
